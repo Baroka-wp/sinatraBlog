9f2024014e20606831d37033bd73ad099bc4638b
baroka <birotori@gmail.com>
2022-08-10 16:00:53 +0100

    Testing Like model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,14 @@
 class Post < ApplicationRecord
   # order posts by created_at DESC (from newest to oldest)
   # default_scope -> { order(created_at: :desc) }
+  after_initialize :init
+
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  validates :text, presence: true, length: { maximum: 250 }
-  validates :title, presence: true
-  validates :commentsCounter,
-            :likessCounter, numericality: { only_integer: true , greater_than_or_equal_to: 0}
+  validates :title, presence: true,length: { maximum: 250 }
+  validates :commentsCounter,:likesCounter, numericality: { only_integer: true , greater_than_or_equal_to: 0}
             
   def update_posts_counter
     user.update(postsCounter: user.posts.count)
@@ -16,5 +16,11 @@ class Post < ApplicationRecord
 
   def five_laster_comments
     comments.order('created_at').last(5)
+  end
+
+  private
+  def init
+    self.commentsCounter ||= 0
+    self.likesCounter ||= 0
   end
 end
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+    user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+    subject { Post.new(title: 'Title', text: 'Body', user_id: user.id) }
+    before { subject.save }
+
+    context 'validations' do
+        it 'is valid with valid attributes' do
+            expect(subject).to be_valid
+        end
+        it 'is not valid without a title' do
+            subject.title = nil
+            expect(subject).to_not be_valid
+        end
+        it 'is not valid with a title exceed 250 characters' do
+            subject.title = 'a' * 251
+            expect(subject).to_not be_valid
+        end
+        it 'is valid without a text' do
+            subject.text = nil
+            expect(subject).to be_valid
+        end
+        it 'is not valid without a user_id' do
+            subject.user_id = nil
+            expect(subject).to_not be_valid
+        end
+        it 'is not valid with commentsCounter in string type' do
+            subject.commentsCounter = "string"
+            expect(subject).to_not be_valid
+        end
+        it 'is not valid with likesCounter in string type' do
+            subject.likesCounter = "string"
+            expect(subject).to_not be_valid
+        end
+    end
+    context 'associations' do
+        it 'has many comments' do
+            assc = described_class.reflect_on_association(:comments)
+            expect(assc.macro).to eq :has_many
+        end
+        it 'has many likes' do
+            assc = described_class.reflect_on_association(:likes)
+            expect(assc.macro).to eq :has_many
+        end
+    end
+    context 'Costum methods' do
+        it 'is showing the three last posts' do
+            expect(subject.five_laster_comments.count).to eq(subject.comments.order('created_at').last(5).count)
+        end
+    end
+
+end
