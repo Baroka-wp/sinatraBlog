b86dd8bbfe8e945b9fdf56d23fc0210011af83c1
GedeonTS <gedeontshobohwa1@gmail.com>
2022-08-23 10:01:47 +0200

    Refactor posts_spec

--- spec/views/posts/posts_spec.rb
+++ spec/views/posts/posts_spec.rb
@@ -29,26 +29,30 @@ RSpec.describe Post, type: :view do
       expect(page).to have_content('Comment by: Tom')
     end
   end
+  
+  context 'Post index page' do
+    it 'Should content Add post button' do
+      visit user_path(@user.id)
+      expect(page).to have_content('Title')
+      expect(page).to have_content('Add post')
+    end
 
-  it 'Should content Add post button' do
-    visit user_path(@user.id)
-    expect(page).to have_content('Title')
-    expect(page).to have_content('Add post')
-  end
 
-  it 'Should display add post form' do
-    visit user_path(@user.id)
-    click_link 'Add post'
-    expect(current_path).to eq(new_post_path)
-    expect(response).to render_template('new')
-  end
+    it 'Should display add post form' do
+      visit user_path(@user.id)
+      click_link 'Add post'
+      expect(current_path).to eq(new_post_path)
+      expect(response).to render_template('new')
+    end
 
-  it 'Should create new post' do
-    visit new_post_path
-    fill_in 'post[title]', with: 'New Title'
-    fill_in 'post[text]', with: 'New Body'
-    click_button 'Submit new post'
-    expect(current_path).to eq(user_posts_path(@user.id))
-    expect(page).to have_content('New Title')
+    it 'Should create new post' do
+      visit new_post_path
+      fill_in 'post[title]', with: 'New Title'
+      fill_in 'post[text]', with: 'New Body'
+      click_button 'Submit new post'
+      expect(current_path).to eq(user_posts_path(@user.id))
+      expect(page).to have_content('New Title')
+    end
   end
 end
+
