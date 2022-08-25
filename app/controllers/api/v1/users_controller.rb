9b00154f9d4864167f1a0d4bfacae5bb80db61e2
baroka <birotori@gmail.com>
2022-08-25 22:34:12 +0100

    build : make api endpoint
    get all users, a user's post and post'scomment

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,15 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
   def index
     @users = User.all
-
     respond_to do |format|
-      format.html # index.html.erb
       format.json { render :json => @users }
     end
   end
 
   def show
     @user = User.find(params[:id])
+    respond_to do |format|
+      format.json { render :json => @user }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,17 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
+
+  # Api routes
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show] do
+        resources :posts, only: [:index, :show, :new]
+      end
+      resources :posts, only: [:new, :create, :destroy] do
+        resources :comments
+        resources :likes
+      end
+    end
+  end
 end
